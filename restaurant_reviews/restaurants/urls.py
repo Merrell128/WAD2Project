03a5957c22657a6fa52b926a154e5db2d447d860
Merrell128 <2310969m@student.gla.ac.uk>
2019-03-03 13:32:14 +0000

    basic templates, views & urls added

--- restaurant_reviews/restaurants/urls.py
+++ restaurant_reviews/restaurants/urls.py
@@ -3,4 +3,13 @@ from restaurants import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+
+	url(r'^about/',views.about, name = 'about'),
+
+	url(r'^register/$',
+        views.register, name='register'),
+
+	url(r'^login/$', views.user_login, name='login'),
+
+
 ]
--- restaurant_reviews/restaurants/views.py
+++ restaurant_reviews/restaurants/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 def index(request):
 	context_msg = { 'boldmessage': 'this is god damn testing'}
 	return render(request, 'restaurants/index.html', context = context_msg)
+
+def about(request):
+	return render(request,'restaurants/about.html')
+
+def register(request):
+	return render(request, 'restaurants/register.html')
+
+def user_login(request):
+	return render(request, 'restaurants/login.html')
+
