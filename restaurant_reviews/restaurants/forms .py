5aab4223c118cdcabd591f22ebed1e59343b1723
Merrell128 <2310969m@student.gla.ac.uk>
2019-03-08 15:55:55 +0000

    forms and models updated

---
+++ restaurant_reviews/restaurants/forms .py
@@ -0,0 +1,41 @@
+from django import forms
+from restaurants.models import Restaurant, Review, UserProfile
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class RestaurantForm(forms.ModelForm):
+	name = forms.CharField(max_length=3,
+	help_text="Please enter the name of the restaurant.")
+	address = forms.CharField(max_length=100,
+	help_text="Please enter the address of the restaurant")
+	telepohne = forms.IntegerField(help_text="Please enter the phone number of the restaurant if there is one")
+	
+	
+	class Meta:
+		model = RestaurantForm
+		fields = ('name', 'picture','cuisine')
+
+class ReviewForm(forms.ModelForm):
+	rating = forms.IntegerField(help_text = 'Enter a value from 1 to 5')
+	review = forms.CharField(required = False)
+	
+	class Meta:
+		model = ReviewForm
+		
+		fields = '__all__'
+		
+		
+class UserForm(forms.ModelForm):
+	password = forms.CharField(widget=forms.PasswordInput())
+	
+	class Meta:
+		model = UserForm
+		
+		fields = ('username', 'email', 'password')
+		
+class UserProfileForm(forms.ModelForm):
+	owner = forms.BooleanField(help_text = 'Check if you are an owner of a restaurant')
+	
+	class Meta:
+		model = UserProfileForm
+		
+		fields = ('owner', 'picture')
---
+++ restaurant_reviews/restaurants/models .py
@@ -0,0 +1,43 @@
+from django.db import models
+from datetime import datetime
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Restaurant(models.Model):
+	name = models.CharField(max_length=30)
+	address = models.CharField(max_length=100, unique=True)
+	telephone = models.IntegerField(max_length=14)
+	Cuisine = models.CharField(max_length=30)
+	picture = models.FileField(upload_to='restaurants_uploads/')
+	slug = models.SlugField(unique=True)
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
+
+	class Meta:
+		verbose_name_plural = 'restaurants'
+
+	def __str__(self):
+		return self.name
+	
+class Review(models.Model):
+	idReview = (('Restaurant.idNumber', 'UserProfile.user.username'))	
+	username = User.username
+	date = models.DateTimeField(default=datetime.now, blank=True)
+	rating = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+	review = models.CharField(max_length=300)
+
+	def __str__(self):
+		return self.question_text
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	website = models.URLField
+	picture = models.FileField(upload_to='profile_images', blank=True)
+	owner = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.user.username
+        
